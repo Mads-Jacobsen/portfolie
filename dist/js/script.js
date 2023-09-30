07062d9905fcfdf2d7a11b85e3a0ca56e1a685ec
Mads Jacobsen <mail@mads-jacobsen.dk>
2023-09-30 19:57:46 +0200

    init commit

---
+++ dist/js/script.js
@@ -0,0 +1,48 @@
+/* Background effekt med bolde */
+const colors = ["#ffbe0b", "#fb5607", "#ff006e", "#8338ec", "#3a86ff"];
+
+const numBalls = 200;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 300)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
+
+const toggleBtn = document.getElementsByClassName('toggle-button')[0]
+const navBarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleBtn.addEventListener('click', () => {
+  navBarLinks.classList.toggle('active')
+})
